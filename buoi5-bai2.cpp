f552320a49d38081498029f95223c67ef8564d48
BuiVuTriAn <trian3112007@gmail.com>
2026-08-10 10:27:20 +0700

    Bai tap 2

---
+++ buoi5-bai2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    const double PI = 3.14159;
+    float r;
+    cout<<"Nhap ban kinh: ";
+    cin >> r;
+    cout<<"Chu vi hinh tron: "<<2*PI*r<<endl;
+    cout<<"Dien tich hinh tron: "<<PI*r*r<<endl;
+    return 0;
+
+}
