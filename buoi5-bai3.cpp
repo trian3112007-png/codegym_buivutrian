f3a0c99a0644d2053807d773ecbe473041c8976f
BuiVuTriAn <trian3112007@gmail.com>
2026-08-10 10:28:09 +0700

    Bai tap 3

---
+++ buoi5-bai3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int tuoi;
+    long thunhap;
+    cout<<"Nhap tuoi: ";
+    cin>>tuoi;
+    cout<<"Nhap thu nhap: ";
+    cin>>thunhap;
+    cout<<(tuoi >= 18) && (thunhap >= 5000000);
+    return 0;
+
+}
