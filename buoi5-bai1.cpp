0442ed72164352d7446db92a4c6d07d88614b1bf
BuiVuTriAn <trian3112007@gmail.com>
2026-08-10 10:25:08 +0700

    first commit

---
+++ buoi5-bai1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a=3, b=5, c=2;
+    int tong = a+b+c;
+    cout<< "Tong la "<< tong <<endl;
+    cout<< "Gia tri trung binh la "<< float(tong)/3<<endl;
+    return 0;
+}
