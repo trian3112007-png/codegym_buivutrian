4a5ee850e5b53bf388e0bb9fab79d1594226c06d
BuiVuTriAn <trian3112007@gmail.com>
2026-08-18 23:35:32 +0700

    Bai 9.1

--- bai1b9.cpp
+++ bai1b9.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 int main(){
     int n,max = 0,dem=0;
-    cout<<"Nhap so nguyen n: ";
+    do{
+    cout<<"Nhap 1 so nguyen duong n: ";
     cin>>n;
+    if (n<=0){
+        cout<<"n khong la so nguyen duong. Vui long nhap lai!"<<endl;
+        cin>>n;
+    }
+    }while(n<=0);
+
     while (n>0){
         dem ++;
-        n = n / 10;
         if (n%10 >max){
             max = n%10;
         }
+        n = n/10;
     }
     cout<<"So chu so la: "<<dem<<endl;
     cout<<"Chu so lon nhat la: "<<max<<endl;
